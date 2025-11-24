8d8e7776c48dddacfb8e7e23d47ea5b85c52c9b4
Silvermax12 <alealabafemi01@gmail.com>
2025-11-24 19:34:13 +0100

    Add explore_controller.g.dart - Generated explore controller

---
+++ lib/features/explore/presentation/explore_controller.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'explore_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$exploreControllerHash() => r'a5d2427f3aa0925674b35bdf9d05ec6f4ecbbeeb';
+
+/// See also [ExploreController].
+@ProviderFor(ExploreController)
+final exploreControllerProvider = AutoDisposeAsyncNotifierProvider<
+  ExploreController,
+  List<Map<String, dynamic>>
+>.internal(
+  ExploreController.new,
+  name: r'exploreControllerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$exploreControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ExploreController =
+    AutoDisposeAsyncNotifier<List<Map<String, dynamic>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
