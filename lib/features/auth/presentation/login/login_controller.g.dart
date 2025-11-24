dda2e60c7ec22f1774a1d1ed5bcc73660f4c494f
Silvermax12 <alealabafemi01@gmail.com>
2025-11-24 19:33:57 +0100

    Add login_controller.g.dart - Generated login controller

---
+++ lib/features/auth/presentation/login/login_controller.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginControllerHash() => r'fdd3a58e6a2e799a127ac9e3b6de70e3bd32f3be';
+
+/// See also [LoginController].
+@ProviderFor(LoginController)
+final loginControllerProvider = AutoDisposeNotifierProvider<
+  LoginController,
+  AsyncValue<AuthDestination?>
+>.internal(
+  LoginController.new,
+  name: r'loginControllerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$loginControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LoginController = AutoDisposeNotifier<AsyncValue<AuthDestination?>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
