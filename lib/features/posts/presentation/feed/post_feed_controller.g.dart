dd16f0d91fbeeb721f4b50850c68a75a2ab95bb4
Silvermax12 <alealabafemi01@gmail.com>
2025-11-24 19:35:16 +0100

    Add post_feed_controller.g.dart - Generated post feed controller

---
+++ lib/features/posts/presentation/feed/post_feed_controller.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_feed_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$postFeedControllerHash() =>
+    r'48bb1cd2b638e0c52bacebcb60496143e82241e1';
+
+/// See also [PostFeedController].
+@ProviderFor(PostFeedController)
+final postFeedControllerProvider =
+    AutoDisposeNotifierProvider<PostFeedController, PostFeedState>.internal(
+      PostFeedController.new,
+      name: r'postFeedControllerProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$postFeedControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$PostFeedController = AutoDisposeNotifier<PostFeedState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
