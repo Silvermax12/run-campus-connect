c30ba65653ddbc4038d6f117109f9216f1c5fb0a
Silvermax12 <alealabafemi01@gmail.com>
2025-11-24 19:35:38 +0100

    Add profile_repository.g.dart - Generated profile repository

---
+++ lib/features/profile/data/profile_repository.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$profileRepositoryHash() => r'4fb820757ba447b7f8e1705587bc367727fba98b';
+
+/// See also [profileRepository].
+@ProviderFor(profileRepository)
+final profileRepositoryProvider = Provider<ProfileRepository>.internal(
+  profileRepository,
+  name: r'profileRepositoryProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$profileRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ProfileRepositoryRef = ProviderRef<ProfileRepository>;
+String _$currentUserProfileHash() =>
+    r'a414dd1a319d44c18f49de1d2d9b345ea35cc537';
+
+/// See also [currentUserProfile].
+@ProviderFor(currentUserProfile)
+final currentUserProfileProvider = StreamProvider<UserProfile?>.internal(
+  currentUserProfile,
+  name: r'currentUserProfileProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$currentUserProfileHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CurrentUserProfileRef = StreamProviderRef<UserProfile?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
