b74cface5d5fffb4523eeb63f99b65ecfd8d2b80
Silvermax12 <alealabafemi01@gmail.com>
2025-11-24 19:35:20 +0100

    Add create_post_controller.g.dart - Generated create post controller

---
+++ lib/features/posts/presentation/create_post/create_post_controller.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'create_post_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$createPostControllerHash() =>
+    r'993d0d8641d12412f6e967233ad2589fc4eb96f6';
+
+/// See also [CreatePostController].
+@ProviderFor(CreatePostController)
+final createPostControllerProvider = AutoDisposeNotifierProvider<
+  CreatePostController,
+  rpd.AsyncValue<void>
+>.internal(
+  CreatePostController.new,
+  name: r'createPostControllerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$createPostControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CreatePostController = AutoDisposeNotifier<rpd.AsyncValue<void>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
