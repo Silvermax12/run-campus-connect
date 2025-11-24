d421eacd04567bc9baad4bb667446f1bdef87614
Silvermax12 <alealabafemi01@gmail.com>
2025-11-24 19:35:27 +0100

    Add comment_controller.g.dart - Generated comment controller

---
+++ lib/features/posts/presentation/comments/comment_controller.g.dart
@@ -0,0 +1,166 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'comment_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$commentsHash() => r'5a5e2d6411acde3120e9b2d41ac4d8db17b34ee4';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [comments].
+@ProviderFor(comments)
+const commentsProvider = CommentsFamily();
+
+/// See also [comments].
+class CommentsFamily extends Family<AsyncValue<List<Comment>>> {
+  /// See also [comments].
+  const CommentsFamily();
+
+  /// See also [comments].
+  CommentsProvider call({required String postId}) {
+    return CommentsProvider(postId: postId);
+  }
+
+  @override
+  CommentsProvider getProviderOverride(covariant CommentsProvider provider) {
+    return call(postId: provider.postId);
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'commentsProvider';
+}
+
+/// See also [comments].
+class CommentsProvider extends AutoDisposeStreamProvider<List<Comment>> {
+  /// See also [comments].
+  CommentsProvider({required String postId})
+    : this._internal(
+        (ref) => comments(ref as CommentsRef, postId: postId),
+        from: commentsProvider,
+        name: r'commentsProvider',
+        debugGetCreateSourceHash:
+            const bool.fromEnvironment('dart.vm.product')
+                ? null
+                : _$commentsHash,
+        dependencies: CommentsFamily._dependencies,
+        allTransitiveDependencies: CommentsFamily._allTransitiveDependencies,
+        postId: postId,
+      );
+
+  CommentsProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.postId,
+  }) : super.internal();
+
+  final String postId;
+
+  @override
+  Override overrideWith(
+    Stream<List<Comment>> Function(CommentsRef provider) create,
+  ) {
+    return ProviderOverride(
+      origin: this,
+      override: CommentsProvider._internal(
+        (ref) => create(ref as CommentsRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        postId: postId,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeStreamProviderElement<List<Comment>> createElement() {
+    return _CommentsProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is CommentsProvider && other.postId == postId;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, postId.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin CommentsRef on AutoDisposeStreamProviderRef<List<Comment>> {
+  /// The parameter `postId` of this provider.
+  String get postId;
+}
+
+class _CommentsProviderElement
+    extends AutoDisposeStreamProviderElement<List<Comment>>
+    with CommentsRef {
+  _CommentsProviderElement(super.provider);
+
+  @override
+  String get postId => (origin as CommentsProvider).postId;
+}
+
+String _$commentControllerHash() => r'5a93295f4ad76b97a12b46e5d46eead83a02081d';
+
+/// See also [CommentController].
+@ProviderFor(CommentController)
+final commentControllerProvider =
+    AutoDisposeAsyncNotifierProvider<CommentController, void>.internal(
+      CommentController.new,
+      name: r'commentControllerProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$commentControllerHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$CommentController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
