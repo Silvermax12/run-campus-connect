faabf6c9482eb93923fbab159de5a7b648c6a4c9
Silvermax12 <alealabafemi01@gmail.com>
2025-11-24 19:33:12 +0100

    Add cloudinary_config.dart - Cloudinary service configuration

---
+++ lib/core/config/cloudinary_config.dart
@@ -0,0 +1,5 @@
+class CloudinaryConfig {
+  static const String cloudName = 'de0zo490s';
+  static const String uploadPreset = 'run-campus-connect';
+  static const String uploadUrl = 'https://api.cloudinary.com/v1_1/$cloudName/image/upload';
+}
