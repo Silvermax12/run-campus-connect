296851777ee557083562895811dd1e4be81e376b
Silvermax12 <alealabafemi01@gmail.com>
2026-03-01 18:00:25 +0100

    Update chat domain

--- lib/features/chat/domain/chat.dart
+++ lib/features/chat/domain/chat.dart
@@ -5,21 +5,33 @@ class Chat {
   final List<String> participants;
   final String lastMessage;
   final DateTime lastTime;
+  final Map<String, int> unreadCounts;
 
   Chat({
     required this.id,
     required this.participants,
     required this.lastMessage,
     required this.lastTime,
+    this.unreadCounts = const {},
   });
 
   factory Chat.fromSnapshot(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
+    final unreadCountsData = data['unreadCounts'] as Map<String, dynamic>?;
+    final unreadCounts = <String, int>{};
+    
+    if (unreadCountsData != null) {
+      unreadCountsData.forEach((key, value) {
+        unreadCounts[key] = (value as num?)?.toInt() ?? 0;
+      });
+    }
+    
     return Chat(
       id: doc.id,
       participants: List<String>.from(data['participants'] as List? ?? []),
       lastMessage: data['lastMessage'] as String? ?? '',
       lastTime: (data['lastTime'] as Timestamp?)?.toDate() ?? DateTime.now(),
+      unreadCounts: unreadCounts,
     );
   }
 }
