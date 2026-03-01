6c6a5e7b76ae66b98f556fb418c685b59a86b214
Silvermax12 <alealabafemi01@gmail.com>
2026-03-01 18:03:54 +0100

    Add generated fresher_controller

---
+++ lib/features/auth/presentation/fresher/fresher_controller.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fresher_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fresherControllerHash() => r'0e32528ef4e10c43f8afa322e3280e90b9f484cd';
+
+/// See also [FresherController].
+@ProviderFor(FresherController)
+final fresherControllerProvider = AutoDisposeNotifierProvider<
+  FresherController,
+  AsyncValue<AuthDestination?>
+>.internal(
+  FresherController.new,
+  name: r'fresherControllerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$fresherControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FresherController = AutoDisposeNotifier<AsyncValue<AuthDestination?>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
