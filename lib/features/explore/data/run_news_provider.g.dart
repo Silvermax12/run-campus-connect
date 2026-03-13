0c35e2c14d81838d8417bda3e0d39cc5a949b7ce
Silvermax12 <alealabafemi01@gmail.com>
2026-03-13 15:26:07 +0100

    add run_news_provider.g.dart

---
+++ lib/features/explore/data/run_news_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'run_news_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$runNewsHash() => r'f52ffef484f97511500d272d93015152634c42ca';
+
+/// See also [runNews].
+@ProviderFor(runNews)
+final runNewsProvider =
+    AutoDisposeStreamProvider<List<Map<String, dynamic>>>.internal(
+      runNews,
+      name: r'runNewsProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product') ? null : _$runNewsHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RunNewsRef = AutoDisposeStreamProviderRef<List<Map<String, dynamic>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
