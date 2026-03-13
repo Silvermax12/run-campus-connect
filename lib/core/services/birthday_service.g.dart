e779541ece764ca3294d9045adc2b6e8d0e3443f
Silvermax12 <alealabafemi01@gmail.com>
2026-03-13 15:22:58 +0100

    add birthday_service.g.dart

---
+++ lib/core/services/birthday_service.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'birthday_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$birthdayServiceHash() => r'59645ccb6385bd0337262ae784a8992a1500a5c7';
+
+/// See also [birthdayService].
+@ProviderFor(birthdayService)
+final birthdayServiceProvider = Provider<BirthdayService>.internal(
+  birthdayService,
+  name: r'birthdayServiceProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$birthdayServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef BirthdayServiceRef = ProviderRef<BirthdayService>;
+String _$isTodayUserBirthdayHash() =>
+    r'930de3703c35d51f31b36f43545941b3a64c6d1c';
+
+/// Provider that checks if the current user's birthday is today.
+///
+/// Copied from [isTodayUserBirthday].
+@ProviderFor(isTodayUserBirthday)
+final isTodayUserBirthdayProvider = AutoDisposeProvider<bool>.internal(
+  isTodayUserBirthday,
+  name: r'isTodayUserBirthdayProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$isTodayUserBirthdayHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef IsTodayUserBirthdayRef = AutoDisposeProviderRef<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
