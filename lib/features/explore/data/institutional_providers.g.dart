c6434c224765d7b4dac08c24ff02cfc30112d624
Silvermax12 <alealabafemi01@gmail.com>
2026-03-13 15:25:47 +0100

    add institutional_providers.g.dart

---
+++ lib/features/explore/data/institutional_providers.g.dart
@@ -0,0 +1,97 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'institutional_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$runHistoryHash() => r'9d0df344796e62b50539d6ad10d807856b5fade2';
+
+/// Fetches the single document from `run_our_history` collection.
+///
+/// Copied from [runHistory].
+@ProviderFor(runHistory)
+final runHistoryProvider =
+    AutoDisposeFutureProvider<Map<String, dynamic>?>.internal(
+      runHistory,
+      name: r'runHistoryProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$runHistoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RunHistoryRef = AutoDisposeFutureProviderRef<Map<String, dynamic>?>;
+String _$runGovernanceHash() => r'9c9a42db8b85ec13c15ad2771e69fd0d78057427';
+
+/// Fetches the single document from `run_governance` collection.
+///
+/// Copied from [runGovernance].
+@ProviderFor(runGovernance)
+final runGovernanceProvider =
+    AutoDisposeFutureProvider<Map<String, dynamic>?>.internal(
+      runGovernance,
+      name: r'runGovernanceProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$runGovernanceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RunGovernanceRef = AutoDisposeFutureProviderRef<Map<String, dynamic>?>;
+String _$runMottoLogoAnthemHash() =>
+    r'8f166a331e76334c6fd4eedb92a1b46099dd6084';
+
+/// Fetches the single document from `run_motto_logo_anthem` collection.
+///
+/// Copied from [runMottoLogoAnthem].
+@ProviderFor(runMottoLogoAnthem)
+final runMottoLogoAnthemProvider =
+    AutoDisposeFutureProvider<Map<String, dynamic>?>.internal(
+      runMottoLogoAnthem,
+      name: r'runMottoLogoAnthemProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$runMottoLogoAnthemHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RunMottoLogoAnthemRef =
+    AutoDisposeFutureProviderRef<Map<String, dynamic>?>;
+String _$runVisionMissionHash() => r'a445099110e119124c43046b7ccc8d5dcfcbfdb4';
+
+/// Fetches the single document from `run_vision_mission` collection.
+///
+/// Copied from [runVisionMission].
+@ProviderFor(runVisionMission)
+final runVisionMissionProvider =
+    AutoDisposeFutureProvider<Map<String, dynamic>?>.internal(
+      runVisionMission,
+      name: r'runVisionMissionProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$runVisionMissionHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RunVisionMissionRef =
+    AutoDisposeFutureProviderRef<Map<String, dynamic>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
