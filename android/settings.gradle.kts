12464deab9bcc7e6e8821c1de65d718bf9fb7278
Silvermax12 <alealabafemi01@gmail.com>
2026-07-01 23:14:38 +0100

    build(android): update settings.gradle dependency resolution

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -21,6 +21,7 @@ plugins {
     id("com.android.application") version "8.7.0" apply false
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version("4.3.15") apply false
+    id("com.google.firebase.firebase-perf") version "2.0.2" apply false
     // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "2.3.20" apply false
 }
